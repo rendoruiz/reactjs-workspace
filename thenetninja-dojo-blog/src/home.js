635edcfbf6b99ecafb1ee9c2f41945f3a766113e
rendoruiz <rendo.ruiz@outlook.com>
2021-08-01 11:02:36 -0600

    #17 fetch
    npx json-server --watch data/db.json --port 8000
    
    logical and &&

--- thenetninja-dojo-blog/src/home.js
+++ thenetninja-dojo-blog/src/home.js
@@ -3,28 +3,21 @@ import BlogList from './BlogList';
 
 const Home = () => {
   
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
-
-  const [name, setName] = useState('rendo');
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log('use effect ran');
-  }, [name]);
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data)
+      })
+  }, []);
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
-      <button onClick={() => setName('odner')}>change name</button>
-      <p>{ name }</p>
+      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
     </div>
    );
 }
