f4d2fb381999b0bb59dcf211d7c2896d4f1d9124
rendoruiz <rendo.ruiz@outlook.com>
2021-08-01 11:24:04 -0600

    #19 error messages

--- thenetninja-dojo-blog/src/home.js
+++ thenetninja-dojo-blog/src/home.js
@@ -5,20 +5,30 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    fetch('http://localhost:8000/blogsz')
       .then(res => {
+        if (!res.ok) {
+          throw Error('Could not fetch data from server.');
+        }
         return res.json()
       })
       .then(data => {
         setBlogs(data)
         setIsLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
       })
   }, []);
 
   return ( 
     <div className="home">
+      { error && <div>{ error }</div>}
       { isLoading && <div>Loading...</div>}
       {(blogs && <BlogList blogs={blogs} title='All Blogs!' />)}
     </div>
