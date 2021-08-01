9d60ae385ac2eedadcab7d6cb92947c476766188
rendoruiz <rendo.ruiz@outlook.com>
2021-08-01 13:04:17 -0600

    #30 useHistory, programmatic redirects

--- thenetninja-dojo-blog/src/Create.js
+++ thenetninja-dojo-blog/src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('odner');
   const [isLoading, setIsLoading] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,6 +21,8 @@ const Create = () => {
     }).then(() => {
       console.log('new blgo added');
       setIsLoading(false);
+      // history.go(-1);
+      history.push('/');
     })
   }
 
