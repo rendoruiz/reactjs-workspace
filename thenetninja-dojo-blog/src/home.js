3b7ae26af7584f29f9e67b8ac0fca9fc2ec813dc
rendoruiz <rendo.ruiz@outlook.com>
2021-07-31 19:13:03 -0600

    #15 useEffect dependencies
    use "[]" (empty array) if useEffect should only run once.
    add a stateful variable inside the array to only specify which one to watch.

--- thenetninja-dojo-blog/src/home.js
+++ thenetninja-dojo-blog/src/home.js
@@ -9,6 +9,8 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ]);
 
+  const [name, setName] = useState('rendo');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -16,11 +18,13 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-  });
+  }, [name]);
 
   return ( 
     <div className="home">
       <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
+      <button onClick={() => setName('odner')}>change name</button>
+      <p>{ name }</p>
     </div>
    );
 }
