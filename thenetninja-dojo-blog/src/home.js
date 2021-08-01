8bef9251ebad5ba7ffd2c9d9f101bcf9b348d088
rendoruiz <rendo.ruiz@outlook.com>
2021-07-31 18:34:38 -0600

    #6 click events

--- thenetninja-dojo-blog/src/home.js
+++ thenetninja-dojo-blog/src/home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log('hello!', e);
+  }
+
+  const handleClickAgain = (name, e) => {
+    console.log('hello! ' + name, e.target);
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('heyyo', e)}>Click me again</button>
     </div>
    );
 }
