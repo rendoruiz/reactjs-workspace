bd87be4274984a1b8847df42fa8f77f1342697d3
rendoruiz <rendo.ruiz@outlook.com>
2021-07-31 18:56:38 -0600

    #11 props vA

--- thenetninja-dojo-blog/src/home.js
+++ thenetninja-dojo-blog/src/home.js
@@ -1,20 +1,17 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
-  // variable and setter
-  const [name, setName] = useState('rendo');
-  const [age, setAge] = useState(10);
-
-  const handleClick = () => {
-    setName('odner')
-    setAge(20)
-  }
+  
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title={'All Blogs!'} />
     </div>
    );
 }
