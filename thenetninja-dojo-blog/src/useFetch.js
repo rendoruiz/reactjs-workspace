dae8c4506b0bfb4e72e7f8055a896845d073ce2d
rendoruiz <rendo.ruiz@outlook.com>
2021-08-01 12:20:06 -0600

    #24 abortcontroller for fetch
    abort controller

--- thenetninja-dojo-blog/src/useFetch.js
+++ thenetninja-dojo-blog/src/useFetch.js
@@ -6,22 +6,34 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(url)
-      .then(res => {
-        if (!res.ok) {
-          throw Error('Could not fetch data from server.');
-        }
-        return res.json()
-      })
-      .then(data => {
-        setData(data)
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err.message);
-      })
+    const abortController = new AbortController();
+
+    setTimeout(() => {
+      fetch(url, { signal: abortController.signal })
+        .then(res => {
+          if (!res.ok) {
+            throw Error('Could not fetch data from server.');
+          }
+          return res.json()
+        })
+        .then(data => {
+          setData(data)
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === 'AbortError') {
+            console.log('fetch aborted');
+          } else {
+            setIsLoading(false);
+            setError(err.message);
+          }
+        })
+    }, 1000)
+
+    // aborts fetch associated
+    // will cancel the fetch when the page goes to new route white its still fetching
+    return () => abortController.abort();
   }, [url]);
 
   return { data, isLoading, error };
